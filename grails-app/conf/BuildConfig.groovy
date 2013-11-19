ce2ca7de3ef35d6ece1308a1214079e5c9d52c07
davidecavestro <davidecavestro@gmail.com>
2013-11-19 16:52:20 +0100

    Removed tomcat dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -11,8 +11,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:2.2.1",
+        build(":release:2.2.1",
               ":rest-client-builder:1.0.3") {
             export = false
         }
